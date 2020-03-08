87acce115c7de0ab61a1e0d54be54cb35ab34517
pavithrasekar24 <52492606+pavithrasekar24@users.noreply.github.com>
2020-03-08 18:52:13 +0530

    added function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import "./styles.css";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedBranch: [],
+      branch: [{ bName: "velachery", bId: 1 }, { bName: "vijayaNagar", bId: 2 }]
+    };
+  }
+  handleChange = (e, i) => {
+    let branch = {
+      ...i,
+      checked: e.target.checked
+    };
+
+    this.setState({ selectedBranch: branch }, () => {
+      console.log(this.state.selectedBranch);
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.branch.map((i, j) => {
+          return (
+            <div>
+              <input
+                key={j}
+                type="checkbox"
+                name={`selectedBranch${j}`}
+                value={this.state.selectedBranch}
+                onChange={e => {
+                  this.handleChange(e, i);
+                }}
+              />
+              {i.bName}
+              <br />
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
